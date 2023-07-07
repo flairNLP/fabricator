003c92a0bed882e992b508a9bf002eccb5835699
Patrick Haller <patrickhaller40@googlemail.com>
2023-07-07 19:17:08 +0200

    Inital training script for ft on ner

---
+++ evaluation/README.md
@@ -0,0 +1,6 @@
+# Evaluation of LLMs NLP Downstream Task capabilities through instruction-fine-tuning
+
+
+## Dataset
+
+https://instructions.apps.allenai.org
---
+++ evaluation/run_train.sh
@@ -0,0 +1,22 @@
+CUDA_VISIBLE_DEVICES=1,2
+
+
+python train.py \
+    --model_name_or_path "../llama_hf" \
+    --bf16 True \
+    --output_dir dar_llama_big_noinp_clean \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy "no" \
+    --save_strategy "epoch" \
+    --save_total_limit 3 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    # --tf32 True
