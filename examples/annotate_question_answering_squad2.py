7484b6f2e99a89c4acee5793adf7febbb04fc217
Julian Risch <julian.risch@deepset.ai>
2023-07-21 14:06:17 +0200

    sample fewshot examples in loop

--- examples/annotate_question_answering_squad2.py
+++ examples/annotate_question_answering_squad2.py
@@ -13,23 +13,13 @@ from ai_dataset_generator.prompts import TextLabelPrompt
 
 
 def run(arguments):
-
-    # TODO
-    # dataset format: nested column
-    # improve prompts
-    # random sampling for prompts at a later stage within the for loop
-    # randomly sample few shot examples from a set of ten examples within the for loop
-    # auto save after every 50(!?) generates examples
-
     """Generate answers based on a few-shot example from context and question."""
     org = load_dataset(arguments.dataset, split=arguments.split)
-    dataset_all_questions = preprocess_squad_format(org)
+
 
     dataset_name = f"qa-dataset"
-
-    #dataset_generated_loaded = load_dataset("julianrisch/generated-qa-dataset-4", split="train")
-    dataset_answerable_questions = dataset_all_questions.filter(lambda sample: sample['answer'] != '').shuffle()
-    dataset_unanswerable_questions = dataset_all_questions.filter(lambda sample: sample['answer'] == '').shuffle()
+    dataset_answerable_questions = org.filter(lambda sample: sample['answers']['text']).shuffle()
+    dataset_unanswerable_questions = org.filter(lambda sample: not sample['answers']['text']).shuffle()
 
     prompt_node = PromptNode(
         model_name_or_path=arguments.llm, api_key=os.environ.get("OPENAI_API_KEY"), max_length=100
@@ -37,25 +27,33 @@ def run(arguments):
     generator = DatasetGenerator(prompt_node)
 
     question_words = ["Which", "What", "How", "When", "Who", "How many", "Where", "Why"]
-    task_descriptions = ["Given a text, first create a difficult question that can be answered using the text. The question must describe the context of the text. Second, extract the answer to this question from the text. The answer must be word for word exactly as it appears in the text.",
-                         f"You are a student and a teacher is teaching you about a new topic. Ask a short follow-up question about something the teacher hasn't mentioned yet at all. You must not ask something you already know the answer to from the teacher's explanations. You must not ask for further clarification if the teacher already mentioned something in passing. The question should be self-contained. It must not contain the word \"other\" as in \"which other\" or \"what other\". The question should start with one of {random.sample(question_words, 3)}"]
 
     filtered_generated_datasets = []
     filtered_original_datasets = []
+
+
     for index, dataset in enumerate([dataset_answerable_questions, dataset_unanswerable_questions]):
-        fewshot_examples = dataset.select([1, 2, 3])
-        unlabeled_examples = dataset.select(range(4, arguments.num_labels + 4, 1))
-
-        prompt = TextLabelPrompt(
-            input_variables=arguments.input_variables,
-            target_variable=arguments.target_variable,
-            task_description=task_descriptions[index],
-        )
-
-        raw_prompt = prompt.get_prompt_text(fewshot_examples)
-        print(raw_prompt)
+        preprocessed_dataset = preprocess_squad_format(dataset)
+        fewshot_examples = preprocessed_dataset.select(range(10))
+        labels_to_generate = arguments.num_labels//3*2 if index == 0 else arguments.num_labels//3
+        unlabeled_examples = preprocessed_dataset.select(range(10, labels_to_generate + 10, 1))
 
         for i in range(0, len(unlabeled_examples), arguments.save_steps):
+            fewshot_examples = fewshot_examples.shuffle().select(range(3))
+
+            task_descriptions = [
+                "Given a text, first create a difficult question that can be answered using the text. The question must describe the context of the text. Second, extract the answer to this question from the text. The answer must be word for word exactly as it appears in the text.",
+                f"You are a student and a teacher is teaching you about a new topic. Ask a short follow-up question about something the teacher hasn't mentioned yet at all. You must not ask something you already know the answer to from the teacher's explanations. You must not ask for further clarification if the teacher already mentioned something in passing. The question should be self-contained. It must not contain the word \"other\" as in \"which other\" or \"what other\". The question should start with one of {random.sample(question_words, 3)}"]
+
+            prompt = TextLabelPrompt(
+                input_variables=arguments.input_variables,
+                target_variable=arguments.target_variable,
+                task_description=task_descriptions[index],
+            )
+
+            raw_prompt = prompt.get_prompt_text(fewshot_examples)
+            print(raw_prompt)
+
             current_unlabeled_examples = unlabeled_examples.select(range(i, min(len(unlabeled_examples), i + arguments.save_steps)))
             generated_dataset, original_dataset = generator.generate(
                 support_examples=fewshot_examples,
@@ -79,37 +77,32 @@ def run(arguments):
                 indices_to_keep = \
                 generated_dataset.map(lambda example, idx: {'idx': idx if example['answers']['answer_start'] == [] else -1},
                                       with_indices=True)['idx']
-            original_dataset = postprocess_squad_format(original_dataset, add_answer_start=False)
             indices_to_keep = [i for i in indices_to_keep if i != -1]
-            # TODO default representation of unanswerable is { "text": [], "answer_start": [] } sequence https://huggingface.co/datasets/squad_v2/viewer/squad_v2/train?row=130300
 
             generated_dataset = generated_dataset.select(indices_to_keep)
             original_dataset = original_dataset.select(indices_to_keep)
-
 
             # add id and title to generated dataset
             generated_dataset = generated_dataset.add_column("id", original_dataset['id'])
             generated_dataset = generated_dataset.add_column("title", original_dataset['title'])
 
+            ids_to_keep = set(original_dataset['id'])
+            original_dataset = dataset.filter(lambda example: example['id'] in ids_to_keep)
+
             features = generated_dataset.features
             features["answers"] = Sequence(feature={'text': Value(dtype='string', id=None), 'answer_start': Value(dtype='int64', id=None)}, length=-1, id=None)
             generated_dataset = generated_dataset.cast(features)
 
-            features = original_dataset.features
-            features["answers"] = Sequence(feature={'text': Value(dtype='string', id=None), 'answer_start': Value(dtype='int64', id=None)}, length=-1, id=None)
-            original_dataset = original_dataset.cast(features)
-
             filtered_generated_datasets.append(generated_dataset)
             filtered_original_datasets.append(original_dataset)
 
-
             filtered_generated_concatenated_dataset = concatenate_datasets(filtered_generated_datasets)
             filtered_generated_concatenated_dataset.save_to_disk(f"{dataset_name}-generated-{index}-{i * arguments.save_steps}")
-            #filtered_original_concatenated_dataset = concatenate_datasets(filtered_original_datasets)
-            #filtered_original_concatenated_dataset.save_to_disk(f"{dataset_name}-original-{index}-{i * arguments.save_steps}")
+            filtered_original_concatenated_dataset = concatenate_datasets(filtered_original_datasets)
+            filtered_original_concatenated_dataset.save_to_disk(f"{dataset_name}-original-{index}-{i * arguments.save_steps}")
     if arguments.push_to_hub:
-        filtered_generated_concatenated_dataset.push_to_hub(dataset_name + "-generated", private=True)
-        #filtered_original_concatenated_dataset.push_to_hub(dataset_name + "-original", private=True)
+        filtered_generated_concatenated_dataset.push_to_hub(f"{dataset_name}-generated-{len(filtered_generated_concatenated_dataset)}", private=False)
+        filtered_original_concatenated_dataset.push_to_hub(f"{dataset_name}-original-{len(filtered_original_concatenated_dataset)}", private=False)
 
 
 if __name__ == "__main__":
