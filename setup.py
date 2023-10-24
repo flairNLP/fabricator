a605a398c4a2836ca27ca30ce21ed197ce3cc226
whoisjones <jonas.golde@gmail.com>
2023-10-24 11:25:07 +0100

    prepare setup.py for v0.1.1

--- setup.py
+++ setup.py
@@ -8,9 +8,15 @@ def requirements():
 
 setup(
     name='fabricator',
-    version='0.1',
+    version='0.1.1',
     author='Humboldt University Berlin, deepset GmbH',
-    description='Generate datasets with large language models.',
+    author_email="goldejon@informatik.hu-berlin.de",
+    description='Conveniently generating datasets with large language models.',
+    long_description="If you require textual datasets for specific tasks, you can utilize large language models "
+                     "that possess immense generation capability. fabricator enables you to conveniently create "
+                     "or annotate datasets to fine-tune your custom model. fabricator is constructed on "
+                     "deepset's haystack and huggingface's datasets libraries, to seamlessly integrate "
+                     "into existing NLP frameworks.",
     package_dir={"": "src"},
     packages=find_packages("src"),
     license="Apache 2.0",
