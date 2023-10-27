5c300a21b8137aac2f4a71645acffff944a5c71c
whoisjones <jonas.golde@gmail.com>
2023-10-27 19:14:04 +0200

    remove spacy dependency: - refactorings of dataset transformations for NER - corresponding adjustments of tests - remove dependency from requirements.txt - remove of unnecessary function replace_token_labels - can be done with dataset transformation function - adjustments of tutorials

--- src/fabricator/dataset_transformations/__init__.py
+++ src/fabricator/dataset_transformations/__init__.py
@@ -7,9 +7,8 @@ __all__ = [
     "replace_class_labels",
     "convert_token_labels_to_spans",
     "convert_spans_to_token_labels",
-    "replace_token_labels",
 ]
 
 from .question_answering import preprocess_squad_format, postprocess_squad_format, calculate_answer_start
 from .text_classification import convert_label_ids_to_texts, get_labels_from_dataset, replace_class_labels
-from .token_classification import convert_token_labels_to_spans, convert_spans_to_token_labels, replace_token_labels
+from .token_classification import convert_token_labels_to_spans, convert_spans_to_token_labels
